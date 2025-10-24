3cc08c0a352c785501fdc91cf4b9397ed321146d
braulio sarabia <sarabiabraulio22@gmail.com>
2025-10-23 17:34:44 -0700

    practicas anteriores

---
+++ practica 07/rectangulo.py
@@ -0,0 +1,8 @@
+#calcular_area_rectangulo
+def calcular_area_rectangulo (base,altura):
+    return base*altura
+area= calcular_area_rectangulo (10,5)
+print(f"el area del rectangulo es: {area}")
+
+
+
